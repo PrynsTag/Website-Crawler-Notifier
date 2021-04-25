919f1e755e5ea3ef650adf865b542b513f6a6220
PrynsTag <201812311@fit.edu.ph>
2021-04-26 04:17:36 +0800

    Added delay_option.

--- notifier/settings.py
+++ notifier/settings.py
@@ -12,6 +12,10 @@ BOT_NAME = 'notifier'
 SPIDER_MODULES = ['notifier.spiders']
 NEWSPIDER_MODULE = 'notifier.spiders'
 
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 1
+AUTOTHROTTLE_MAX_DELAY = 3
+
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'notifier (+http://www.yourdomain.com)'
@@ -25,7 +29,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
