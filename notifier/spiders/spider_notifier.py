f24cd68c7e864544289a001805c22a10353cdf2c
PrynsTag <201812311@fit.edu.ph>
2021-04-28 07:45:37 +0800

    get project settings for crawler process.

--- notifier/spiders/spider_notifier.py
+++ notifier/spiders/spider_notifier.py
@@ -5,6 +5,7 @@ import ssl
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.http import FormRequest
+from scrapy.utils.project import get_project_settings
 
 
 def send_email(msg):
@@ -44,6 +45,6 @@ class SpiderNotifier(scrapy.Spider):
 
 
 if __name__ == "__main__":
-    process = CrawlerProcess()
+    process = CrawlerProcess(settings=get_project_settings())
     process.crawl(SpiderNotifier)
     process.start()
